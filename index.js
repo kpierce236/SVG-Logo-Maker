d78fea802af603370cc1688b6a1341da7535afed
Kevin Pierce <kpierce236@gmail.com>
2023-12-16 17:11:21 -0500

    added switch statement for shapes classes and then write the rendered shape to file

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
+import { Circle, Triangle, Square } from './lib/shapes.mjs'; 
 import inquirer from 'inquirer';
+import fs from 'fs';
 
-  inquirer
-  .prompt([
+
+// Define the prompts for user input
+const prompts = [
     {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters:',
-      },
-      {
-        type: 'input',
-        message: 'Enter text color:',
-        name: 'textColor',
-      },
-      {
-        type: 'list',
-        message: 'Select a shape:',
-        name: 'shape',
-        choices: ['circle', 'triangle', 'square'],
-      },
-      {
-        type: 'input',
-        message: 'Enter shape color:',
-        name: 'shapeColor',
-      },
-  ])
-  .then((data) => {
-    console.log(`Selections:${data.text},${data.textColor},${data.shape},${data.shapeColor}`);
-  })
-  .catch((error) => {
-    console.error('An error occured: ', error);
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color:',
+      default: 'blue',
+    },
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter the text:',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color:',
+    },
+  ];
+  
+  // Run the prompts to collect user input
+  inquirer.prompt(prompts).then((answers) => {
+    // Determine the shape class based on user input
+    let ShapeClass;
+    switch (answers.shape) {
+      case 'Circle':
+        ShapeClass = Circle;
+        break;
+      case 'Triangle':
+        ShapeClass = Triangle;
+        break;
+      case 'Square':
+        ShapeClass = Square;
+        break;
+      default:
+        console.error('Invalid shape selected.');
+        process.exit(1);
+    }
+  
+    // Create an instance of the selected shape
+    const shape = new ShapeClass(answers.shapeColor,answers.text, answers.textColor);
+  
+    // Write the SVG code to a file named logo.svg
+    fs.writeFileSync('logo.svg', shape.render());
+  
+    console.log('Generated logo.svg');
   });
